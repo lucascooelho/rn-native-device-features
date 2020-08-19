de1821073d42a35e40e8aef7c31058bf9bdd8483
Lucas N. Coelho <lucascooelho@hotmail.com>
2020-08-18 22:00:36 -0300

    CameraAccess/PickImage/Store/Location/Marker/Posit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducer';
+import { init } from './helpers/db';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+init()
+  .then(() => {
+    console.log('Initialized database');
+  })
+  .catch(err => {
+    console.log('Initializing db failed.');
+    console.log(err);
+  });
+
+  const rootReducer = combineReducers({
+    places: placesReducer
+  });
+  
+  const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+  
+  export default function App() {
+    return (
+      <Provider store={store}>
+        <PlacesNavigator />
+      </Provider>
+    );
+  }
+  
---
+++ env.js
@@ -0,0 +1,6 @@
+const vars = {
+    googleApiKey: 'AIzaSyBVp8ONi8X8fY2B_STLwDMxeN4V72y9E3U'
+    // googleApiKey: 'AIzaSyBdKYjcXOiVnAZBvUR-OWCyyG4gUB8JhSM'
+};
+
+export default vars;
